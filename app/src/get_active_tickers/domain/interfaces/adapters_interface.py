45d39df80dc0f527bb0d2590e31760381b92e210
Thiago Panini <panini.development@gmail.com>
2025-02-03 23:49:19 -0300

    config: alterando parâmetro de argumentos posicionais de um método de 5 para 9

--- app/src/get_active_tickers/domain/interfaces/adapters_interface.py
+++ app/src/get_active_tickers/domain/interfaces/adapters_interface.py
@@ -1,11 +1,21 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
+import requests
 from app.src.get_active_tickers.domain.entities.ticker import Ticker
 
-class ITickersInfoAdapter(metaclass=ABCMeta):
+
+class IRequestsAdapter(ABC):
+    """
+    Interface de contrato para requisições HTTP/HTTPs via requests
+    """
+    @abstractmethod
+    def get(self) -> requests.models.Response:
+        raise NotImplementedError
+
+
+class ITickersInfoAdapter(ABC):
     """
     Interface de contrato para coleta de informações de tickers da B3
     """
-
     @abstractmethod
     def get_tickers(self) -> list[Ticker]:
         raise NotImplementedError
