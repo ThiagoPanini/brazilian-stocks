1f63e3e1330d2d0c8f9c02fc66d5e36f8eede9cd
Thiago Panini <panini.development@gmail.com>
2025-02-06 09:02:11 -0300

    feat: adicionando classe de repositório para DynamoDB

--- app/src/get_active_tickers/infra/repositories/dynamodb_repository.py
+++ app/src/get_active_tickers/infra/repositories/dynamodb_repository.py
@@ -1,14 +1,14 @@
 import boto3
 
 from app.src.get_active_tickers.domain.entities.ticker import Ticker
-from app.src.get_active_tickers.domain.interfaces.repositories_inteface import (
+from app.src.get_active_tickers.domain.interfaces.repositories_interface import (
     ITickersInfoRepository
 )
 
 
 class DynamodbTickersInfoRepository(ITickersInfoRepository):
 
-    def __init__(self, table_name: str, region_name: str = "sa-east-1"):
+    def __init__(self, table_name: str, region_name: str):
         self.__table_name: str = table_name
         self.__region_name: str = region_name
         self.__boto3_resource = boto3.resource("dynamodb", region_name=self.__region_name)
